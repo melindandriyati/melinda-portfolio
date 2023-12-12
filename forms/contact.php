0b9fadf34e1d76714310d134f97eea17a79d9183
melindandriyati <melinda.a121@gmail.com>
2023-12-12 08:27:40 +0800

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -54,9 +54,9 @@
   if (!(isset($_POST['message']))) {
     exit("Invalid message!");
   }
-  if ((isset($_POST['first_name']))) {
-    exit("This is spam!");
-  }
+  // if ((isset($_POST['first_name']))) {
+  //   exit("This is spam!");
+  // }
   
   $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_SPECIAL_CHARS);
   $from = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
